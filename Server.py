2204a449a9e6a1262c94d1cfe18a904bdd6d1a16
Estefania Chávez Guardado <fachinacg@gmail.com>
2014-04-23 22:36:15 -0500

    Server manda a controlador de negocio a procesar solicitudes Cliente manda informacion al socket

--- Server.py
+++ Server.py
@@ -1,5 +1,8 @@
 import SocketServer
 import json
+from ActionHandler import ActionHandler
+
+actionHandler = ActionHandler()
 
 class MyTCPServer(SocketServer.ThreadingTCPServer):
     allow_reuse_address = True
@@ -8,12 +11,10 @@ class MyTCPServerHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         try:
             data = json.loads(self.request.recv(1024).strip())
-            # process the data, i.e. print it:
-            print data
-            # send some 'ok' back
-            self.request.sendall(json.dumps({'return':'ok'}))
+            response = actionHandler.procesaAccion(data)
+            self.request.sendall(json.dumps(response))
         except Exception, e:
-            print "Exception wile receiving message: ", e
+            print "Exception while receiving message: ", e
 
 server = MyTCPServer(('127.0.0.1', 13373), MyTCPServerHandler)
 server.serve_forever()
