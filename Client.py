6ba1418f94f47d0aab757fd0e6300281fe91a813
Estefania Chávez Guardado <fachinacg@gmail.com>
2014-06-14 18:00:06 -0500

    Conexion parametrizada

--- Client.py
+++ Client.py
@@ -4,9 +4,9 @@
 import socket
 import json
 
-def sendData(data):
+def sendData(data, serverIP='127.0.0.1', serverPort=13373):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(('127.0.0.1', 13373))
+    s.connect((serverIP, serverPort))
     s.send(json.dumps(data))
     result = json.loads(s.recv(1024))
     s.close()
