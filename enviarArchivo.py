4d9d4544049617b7e528d5fce4b5f8e5b836d5ea
Estefania Chávez Guardado <fachinacg@gmail.com>
2014-06-18 19:12:49 -0500

    Validacion de archivo a enviar

--- enviarArchivo.py
+++ enviarArchivo.py
@@ -2,24 +2,29 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
 import datetime
 
 import Client
 import FileHandler
 
 def main(argumentos):
-    fileName = argumentos[3]
-    mensaje = {
-        "accion" : "archivo",
-        "usuario" : argumentos[1],
-        "informacionMsj" : {
-            "horaFecha" : str(datetime.datetime.now()),
-            "mensaje" : fileName,
-            "archivo": FileHandler.archivoAString(fileName)
+    usuarioDestino = argumentos[1]
+    nombreArchivo = argumentos[2]
+    if os.path.isfile(nombreArchivo):
+        mensaje = {
+            "accion" : "archivo",
+            "usuario" : usuarioDestino,
+            "informacionMsj" : {
+                "horaFecha" : str(datetime.datetime.now()),
+                "mensaje" : os.path.basename(nombreArchivo),
+                "archivo": FileHandler.archivoAString(nombreArchivo)
+            }
         }
-    }
 
-    return Client.sendData(mensaje)
+        return Client.sendData(mensaje)
+
+    raise Exception("Archivo no existe o inválido: " + nombreArchivo)
 
 if __name__ == "__main__":
     print main(sys.argv)
